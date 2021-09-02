39e2c9dde993bbc42be24b6b6668412f06c486fc
tmyroniuk <tmyroniuk@gmail.com>
2021-09-02 13:27:21 +0300

    moved to repo

--- main.py
+++ main.py
@@ -4,52 +4,158 @@ from scipy.integrate import odeint
 from covid19dh import covid19
 import matplotlib.pyplot as plt
 from pyswarms.utils.plotters import (plot_cost_history, plot_contour, plot_surface)
-
-df, _ = covid19("UKR", raw=False, start="2020-03-03", end="2021-02-03")
-df = df.reset_index()
-I_target = np.subtract(df['confirmed'].to_numpy(), np.add(df['recovered'].to_numpy(), df['deaths'].to_numpy()))
-N = df.at[0, 'population']
+from pso import PSO
 
 
-def gof(params):
-    y = ode_solver(params)
+df_ukr = covid19("UKR", raw=False, verbose=False, start="2020-03-03", end="2021-03-01")[0].reset_index()
+df_gbr = covid19("GBR", raw=False, verbose=False, start="2020-03-03", end="2021-03-01")[0].reset_index()
+
+
+def mean_absolute_deviation(params, model, y0, data):
+    y = swarm_ode_solver(model, y0, params, len(data))
     res = 0
-    for t in range(0, len(df)):
-        res += np.abs(np.add(y[:, t, 1], y[:, t, 2]) - I_target[t]) / len(df)
+    for t in range(0, len(data)):
+        res += np.abs(np.add(y[:, t, 2], y[:, t, 1]) - data[t]) / len(data)
     return res
+
+
+def mean_square_deviation(params, model, y0, data):
+    y = swarm_ode_solver(model, y0, params, len(data))
+    res = 0
+    for t in range(0, len(data)):
+        res += np.square(np.add(y[:, t, 2], y[:, t, 1]) - data[t]) / len(data)
+    return res
+
+
+def seir(y, t, params):
+    """ SEIR model
+
+        params:
+        a - absolute population increase (daily)
+        b - relative spread
+        d - exposed recovery rate
+        e1- exposed mortality rate + rate of transfer to infectious
+        e2- infectious mortality + rate of recovery
+        g - exposed rate of becoming infectious
+        l - infectious recovery rate
+        m - normal mortality rate
+    """
+    a, b, d, e1, e2, g, l, m = params
+    S, E, I, R = y
+    return [
+        a - b * S / (S + E + I + R) * I - m * S,
+        b * S / (S + E + I + R) * I - e1 * E,
+        g * E - e2 * I,
+        d * E + l * I - m * R
+    ]
 
 
 def seis(y, t, params):
+    """ SEIR model
+
+            params:
+            a - absolute population increase (daily)
+            b - relative spread
+            e - infectious rate of recovery
+            q - exposed mortality
+            r - infectious recovery rate
+            m - normal mortality rate
+        """
     a, b, m, r, q, e = params
     S, E, I = y
-    return [a - b*S/N*I - m*S + r*I, b*S/N*I - (q + m) * E, e*E - (r + m) * I]
+    return [
+        a - b * S / (S + E + I) * I - m * S + r * I,
+        b * S / (S + E + I) * I - (q + m) * E,
+        e * E - (r + m) * I
+    ]
+
+def seirs(y, t, params):
+    """ SEIRS model, not used
+    """
+    a, b, e, g, m, r, q = params
+    S, E, I, R = y
+    return [
+        a - b * S / (S + E + I + R) * I - m * S + g * R,
+        b * S / (S + E + I + R) * I - (q + m) * E,
+        e * E - (r + m) * I,
+        r * I - (g + m) * R
+    ]
 
 
-def ode_solver(params):
-    res = np.empty((0, len(df), 3))
+def swarm_ode_solver(model, y0, params, n):
+    """
+    Solves ODE for the search-space
+    :param model: ODE
+    :param params: ODE parameters
+    :param n: dimensions
+    :return: numeric solution to given ODE
+    """
+    res = np.empty((0, n, len(y0)))
     for particle in params:
-        res = np.append(res, [odeint(seid, (N-1, 1, 0), np.arange(len(df)), args=(particle,))], axis=0)
+        res = np.append(res, [odeint(model, y0, np.arange(n), args=(particle,))], axis=0)
     return res
 
 
-options = {'c1': 0.3, 'c2': 0.5, 'w': 0.9}
+def display(df, model, deviation, bounds):
+    """ Solves parameter optimization problem for given model and data,
+        plots data via plt. Uses PSO by PySwarms
+    """
+    options = {'c1': 2, 'c2': 2, 'w': 0.9}
+    y0 = (df.at[0, 'population'] - df.at[0, 'confirmed'], 0,  df.at[0, 'confirmed'], 0)
+    data = np.subtract(df['confirmed'].to_numpy(), np.add(df['recovered'].to_numpy(), df['deaths'].to_numpy()))
+    args = {'model': model, 'y0': y0, 'data': data}
 
-# Call instance of PSO
-optimizer = ps.single.GlobalBestPSO(n_particles=150, dimensions=6, options=options, bounds=(np.zeros(6), np.ones(6)))
+    optimizer = ps.single.GlobalBestPSO(n_particles=80, dimensions=len(bounds[0]), options=options, bounds=bounds)
+    cost, pos = optimizer.optimize(deviation, iters=512, **args)
 
-# Perform optimization
-cost, pos = optimizer.optimize(gof, iters=200)
+    t = np.arange(len(data))
+    t5 = np.arange(len(data)*5)
+    sol = odeint(model, y0, t, args=(pos,))
+    plt.plot(t, np.add(sol[:, 2], sol[:, 1]), 'b', label='model data')
+    plt.plot(t, data, 'g', label='real data')
+    plt.legend(loc='best')
+    plt.show()
 
-t = np.arange(len(df))
+    print(cost)
+    print(pos)
 
-print(pos)
+    plot_cost_history(cost_history=optimizer.cost_history)
+    plt.show()
 
-sol = odeint(seird, (N-1, 1, 0), np.arange(len(I_target)), args=(pos,))
+    sol = odeint(model, y0, t5, args=(pos,))
+    plt.plot(t5, np.add(sol[:, 2], sol[:, 1]), 'b', label='model data')
+    plt.plot(t, data, 'g', label='real data')
+    plt.legend(loc='best')
+    plt.show()
 
-plt.plot(t, np.add(sol[:, 2], sol[:, 1]), 'b', label='model')
-plt.plot(t, I_target, 'g', label='target')
-plt.legend(loc='best')
-plt.show()
-plot_cost_history(cost_history=optimizer.cost_history)
-plt.show()
 
+def display_custom(df, model, deviation, bounds):
+    """ Solves parameter optimization problem for given model and data,
+            plots data via plt. Uses PSO from pso.py
+        """
+    y0 = (df.at[0, 'population'] - df.at[0, 'confirmed'], 0,  df.at[0, 'confirmed'])
+    data = np.subtract(df['confirmed'].to_numpy(), np.add(df['recovered'].to_numpy(), df['deaths'].to_numpy()))
+
+    optimizer = PSO(40, 6, bounds, 0.9, 2, 3)
+    cost, pos = optimizer.optimize(deviation, 10, model=model, y0=y0, data=data)
+
+    t = np.arange(len(data))
+    t5 = np.arange(len(data)*5)
+    sol = odeint(model, y0, t, args=(pos,))
+    plt.plot(t, np.add(sol[:, 2], sol[:, 1]), 'b', label='model data')
+    plt.plot(t, data, 'g', label='real data')
+    plt.legend(loc='best')
+    plt.show()
+
+    print(cost)
+    print(pos)
+
+    sol = odeint(model, y0, t5, args=(pos,))
+    plt.plot(t5, np.add(sol[:, 2], sol[:, 1]), 'b', label='model data')
+    plt.plot(t, data, 'g', label='real data')
+    plt.legend(loc='best')
+    plt.show()
+
+
+# display_custom(df_ukr, seis, mean_absolute_deviation, (np.zeros(6), np.array([1024, 1, 1, 1, 1, 1])))
+display(df_ukr, seir, mean_absolute_deviation, (np.zeros(8), np.array([1024, 1, 1, 1, 1, 1, 1, 1])))
---
+++ pso.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+class PSO:
+    def __init__(self, particles, dimensions, bounds, w, c, K):
+        self.w = w                                      # inertial weight
+        self.c = c                                      # acceleration
+        self.K = K                                      # neighborhood size
+        self.S = particles                              # population
+        self.D = dimensions                             # dimensions number
+        self.E = np.array((bounds[0], bounds[1])).T     # search space (boundaries)
+        self.x = np.empty((0, self.D))                  # particle positions
+        self.N = np.empty((self.S, self.K))             # neighborhood graph
+        for i in range(0, self.S):
+            self.x = np.append(self.x, [np.random.uniform(bounds[0], bounds[1])], axis=0)
+        self.v = np.empty((0, self.D))                  # particle velocities
+        for i in range(0, self.S):
+            self.v = np.append(self.v, [np.random.uniform(bounds[0] - self.x[i], bounds[1] - self.x[i])], axis=0)
+        self.p = np.copy(self.x)                        # local best position
+
+    def reset_neighborhood(self):
+        # create new neighborhood topography
+        self.N = np.random.randint(0, self.S, (self.S, self.K))
+        self.l = np.copy(self.p)                        # neighborhood best position
+        self.l_val = np.copy(self.p_val)
+
+        # each particle gives info to all neighbors
+        for i in range(0, self.S):
+            for j in range(0, self.K):
+                if self.p_val[i] < self.l_val[self.N[i][j]]:
+                    self.l[self.N[i][j]] = np.copy(self.p[i])
+
+    def optimize(self, f, iterations, **kwargs):
+        self.x_val = f(self.x, **kwargs)
+        self.p_val = np.copy(self.x_val)
+        self.g_best, self.g_best_val = self.p[np.argmin(self.p_val)], np.min(self.p_val)
+        self.reset_neighborhood()
+        for t in range(0, iterations):
+            updated = False
+
+            # calculate center of gravity
+            G = np.add(self.x, self.c * np.add(self.p, np.subtract(self.l, 2 * self.x)) / 3)
+            for i in range(0, self.S):
+                if np.all(np.equal(self.p[i], self.l[i])):
+                    # for those which store pest neighborhood result
+                    G[i] = np.add(self.x[i], self.c * np.subtract(self.p[i], self.x[i]) / 2)
+
+            # constructing hypersphere of this radius
+            radius = np.sqrt(np.sum(np.square(np.subtract(G, self.x))))
+
+            # x_r is random point inside hypersphere
+            x_r = np.empty((0, self.D))
+            for i in range(0, self.S):
+                temp = np.random.uniform(-1, 1, self.D)
+                r = np.sqrt(np.sum(np.square(temp)))
+                temp = temp / r * np.random.uniform(0, radius)
+                x_r = np.append(x_r, [temp], axis=0)
+
+            # update velocity and position
+            self.v, self.x = np.subtract(np.add(self.w * self.v, x_r), self.x), np.add(self.w * self.v, x_r)
+
+            # check for out-of-bounds particles
+            for i in range(0, self.S):
+                for d in range(0, self.D):
+                    if self.x[i][d] < self.E[d][0]:
+                        self.x[i][d] = self.E[d][0]
+                        self.v[i][d] = -0.5 * self.v[i][d]
+                    elif self.x[i][d] > self.E[d][1]:
+                        self.x[i][d] = self.E[d][1]
+                        self.v[i][d] = -0.5 * self.v[i][d]
+
+            # calculate values for new positions
+            self.x_val = f(self.x, **kwargs)
+            for i in range(0, self.S):
+                if self.x_val[i] < self.p_val[i]:
+                    # new local best found, write it
+                    self.p[i] = np.copy(self.x[i])
+                    self.p_val[i] = self.x_val[i]
+                    if self.g_best_val < self.p_val[i]:
+                        # new global best found, write it
+                        self.g_best = np.copy(self.p[i])
+                        self.g_best_val = self.p_val[i]
+                        updated = True
+                    if self.p_val[i] < self.l_val[i]:
+                        # inform yourself
+                        self.l[i] = np.copy(self.p[i])
+                        self.l_val[i] = self.p_val[i]
+            for i in range(0, self.S):
+                # inform neighbors
+                for j in range(0, self.K):
+                    if self.p_val[i] < self.l_val[self.N[i][j]]:
+                        self.l[self.N[i][j]] = np.copy(self.p[i])
+                        self.l_val[self.N[i][j]] = self.p_val[i]
+            print(t, '/', iterations, ' cost: ', self.g_best_val)
+
+            # no progress, update neighborhood topology
+            if not updated:
+                self.reset_neighborhood()
+        return self.g_best_val, self.g_best
